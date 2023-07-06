103edcf2acf50fccce50ebdac199e389fe020edc
Wojciech Marek <wojciech.marek@xebia.com>
2023-07-06 17:36:57 +0200

    feat: update env

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -2,11 +2,11 @@ import { Layout } from '@musicfy/web/views';
 import { Analytics } from '@vercel/analytics/react';
 
 export function App() {
-  const isProduction = import.meta.env.VITE_NODE_ENV === 'production';
+  const isProduction = process.env.NODE_ENV === 'production';
   console.log('isProduction', isProduction);
 
-  const api = import.meta.env.VITE_API_URL;
-  const key = import.meta.env.VITE_API_KEY;
+  const api = process.env.API_URL;
+  const key = process.env.API_KEY;
   console.log('api|key', api, key);
   
   return (
