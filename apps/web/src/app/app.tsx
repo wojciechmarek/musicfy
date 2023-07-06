5b56ef919ded16f9743f1eebd35751dea6f5ed59
Wojciech Marek <wojciech.marek@xebia.com>
2023-07-06 17:29:25 +0200

    feat: update global env

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,8 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Layout } from '@musicfy/web/views';
 import { Analytics } from '@vercel/analytics/react';
 
 export function App() {
+  const isProduction = import.meta.env.VITE_NODE_ENV === 'production';
+  console.log('isProduction', isProduction);
+
+  const api = import.meta.env.VITE_API_URL;
+  const key = import.meta.env.VITE_API_KEY;
+  console.log('api|key', api, key);
+  
   return (
     <>
       <Analytics />
