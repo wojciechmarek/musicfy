3626069122a0c040733723ce72a3c320f3745738
Wojciech Marek <wojciech.marek@xebia.com>
2023-07-08 01:19:40 +0200

    fix: restore build

--- libs/web/store/vite.config.ts
+++ libs/web/store/vite.config.ts
@@ -2,11 +2,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import { joinPathFragments } from '@nx/devkit';
 
 export default defineConfig({
   cacheDir: '../../../node_modules/.vite/web-store',
 
   plugins: [
+    dts({
+      entryRoot: 'src',
+      tsConfigFilePath: joinPathFragments(__dirname, 'tsconfig.lib.json'),
+      skipDiagnostics: true,
+    }),
     react(),
     viteTsConfigPaths({
       root: '../../../',
@@ -21,6 +28,22 @@ export default defineConfig({
   //    }),
   //  ],
   // },
+
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'web-routing',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forgot to update your package.json as well.
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    },
+  },
 
   test: {
     globals: true,
