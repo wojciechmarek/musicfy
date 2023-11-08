9def439174be7be4c28f3faf761da4f0a5e41cdc
Wojciech Marek <marek.wojciech@outlook.com>
2023-11-08 20:16:42 +0100

    chore: remove logrocket, install vercel

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -2,11 +2,8 @@ import { store } from '@musicfy/web/utility/store';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@musicfy/web/utility/routing';
-import LogRocket from 'logrocket';
 
 export function App() {
-  LogRocket.init('a6lahl/musicfy');
-
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 
 import App from './app/app';
 
+inject();
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
