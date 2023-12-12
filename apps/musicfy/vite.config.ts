2565bbc5cb1ea7ee16976812d43a8078379e87c3
Wojciech Marek <marek.wojciech@outlook.com>
2023-12-12 18:20:07 +0100

    chore: change the app name from web to musicfy

--- apps/musicfy/vite.config.ts
+++ apps/musicfy/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/web',
+  cacheDir: '../../node_modules/.vite/musicfy',
 
   server: {
     port: 4200,
