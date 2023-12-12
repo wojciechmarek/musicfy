ce02c4bbc2e454a6cac68d4f44b94ad50cfdaa43
Wojciech Marek <marek.wojciech@outlook.com>
2023-12-12 18:25:21 +0100

    fix: restore deleted file

---
+++ libs/web/utility/models/src/lib/demo-song.ts
@@ -0,0 +1,10 @@
+export class DemoSong {
+  constructor(
+    public id: number,
+    public title: string,
+    public artist: string,
+    public duration: number,
+    public cover: string,
+    public url: string,
+  ) {}
+}
